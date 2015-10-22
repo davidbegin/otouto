80bd6ce75353cd4eab1fac21d8c7000b64ce3f91
David Begin <davidmichaelbe@gmail.com>
2015-10-21 20:39:51 -0700

    Refactor app internals a little

--- lib/otouto/app.rb
+++ lib/otouto/app.rb
@@ -4,41 +4,50 @@ require "ostruct"
 module Otouto
   class App
     def start
-      hostname = Downup::Base.new(
-        options: hostnames,
-        header_proc: Title.method(:print)
-      ).prompt
+      hostname  = request_hostname
+      route     = request_route(hostname)
+      route     = request_and_add_data_to_route(hostname, route)
 
-      route = Downup::Base.new(
-        options: route_option_generator(hostname),
-        header_proc: Title.method(:print)
-      ).prompt
+      p curl_stmt = "curl " + headers + hostname + route + parse_if_html(@route_obj)
+      sleep 1
+      p system(curl_stmt)
+    end
 
+    private
+
+    def request_and_add_data_to_route(hostname, route)
       route_letter, route_hash = routes(hostname).find { |route_letter, route_hash|
         route_hash.fetch("url") == route
       }
 
-      route_obj = OpenStruct.new(route_hash)
-      data_hash = if route_obj.data.empty?
+      @route_obj = OpenStruct.new(route_hash)
+      data_hash = if @route_obj.data.empty?
                     {}
                   else
-                    route_obj.data.each_with_object({}) do |data_needed, data_hash|
+                    @route_obj.data.each_with_object({}) do |data_needed, data_hash|
                       puts
                       print data_needed.to_s + " > "
                       data_hash[data_needed] = gets.chomp
                     end
                   end
 
-      data_hash.each_pair do |param, value|
-        route.sub!(":#{param}", value)
-      end
-
-      p curl_stmt = "curl " + headers + hostname + route + parse_if_html(route_obj)
-      sleep 1
-      p system(curl_stmt)
+      data_hash.each_pair { |param, value| route.sub!(":#{param}", value) }
+      route
     end
 
-    private
+    def request_hostname
+      Downup::Base.new(
+        options: hostnames,
+        header_proc: Title.method(:print)
+      ).prompt
+    end
+
+    def request_route(hostname)
+      Downup::Base.new(
+        options: route_option_generator(hostname),
+        header_proc: Title.method(:print)
+      ).prompt
+    end
 
     def parse_if_html(route_obj)
       route_obj.content_type == "HTML" ?  " | html2text" : ""
