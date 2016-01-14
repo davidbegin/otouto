b91b8bacedf5ebe1b810124066e7e8b7ccf22a8f
David Begin <davidmichaelbe@gmail.com>
2016-01-13 21:46:26 -0800

    silly mistakes in some comments

--- lib/otouto.rb
+++ lib/otouto.rb
@@ -1,17 +1,21 @@
-require "otouto/version"
-require "otouto/app"
-require "otouto/cli"
-require "otouto/title"
+# require "otouto/version"
+# require "otouto/app"
+# require "otouto/cli"
+# require "otouto/title"
+require_relative "otouto/version"
+require_relative "otouto/app"
+require_relative "otouto/cli"
+require_relative "otouto/title"
 require "fileutils"
 require "downup"
 
 module Otouto
   class << self
-    # base_dir is directory that a new project will generated in
+    # base_dir is the directory that a new project will generated in.
     # it defaults to the current directory
     attr_accessor :base_dir
 
-    # sets the base director back to the current_directory
+    # sets the base directory back to the current directory
     def reset_base_dir!
       @base_dir = ""
     end
